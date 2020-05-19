5bb989abc0031b5538918d1850e8279eebab02a2
Andy Agus <redagus@gmail.com>
2020-05-19 22:21:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,4 +12,10 @@ def loop_message_n_times(message, i)
     puts message
     counter += 1
   end
+
+def output_array(array)
+  counter = 0 
+  while counter < array.length do
+    puts array[i] 
+    counter +=1
 end
